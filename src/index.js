3d9a3eacaa25606bacf60b718ff5b21c2217d3ba
Brad Bohen <bbohen@gmail.com>
2017-05-21 23:09:30 -0400

    Fixed issue with class instance methods returning JSX, added fallback

--- src/index.js
+++ src/index.js
@@ -9,27 +9,42 @@ export default function ({ types: t }) {
       },
       ReturnStatement(path) {
         const { node: { argument } } = path;
-        if (argument.type !== 'JSXElement') {
+        if (argument && argument.type !== 'JSXElement') {
           return;
         }
         const { node: { id: parentId }, parent: functionalComponent } = path.getFunctionParent();
+        const isClassInstanceMethod = parentId && parentId.name;
         const isClassRenderMethod = className && parentId && parentId.name === 'render';
-        let identity = 'unknown';
+        const isFunctionalComponent = functionalComponent && functionalComponent.id;
+        let identity;
 
         if (isClassRenderMethod) {
           identity = className;
-        } else if (functionalComponent) {
+        } else if (isFunctionalComponent) {
           identity = functionalComponent.id.name;
+        } else if (isClassInstanceMethod) {
+          identity = parentId.name;
+        } else {
+          // last ditch effort at looking for a named component
+          // might just want to ignore these entirely
+          const {
+            node: { declarations: [{ name: variableDeclarationName }] },
+          } = path.findParent(pp => pp.isVariableDeclaration());
+          identity = variableDeclarationName;
         }
 
-        const newAttributes = [
-          t.jSXAttribute(
-            t.jSXIdentifier('data-component-id'),
-            t.stringLiteral(identity),
-          ),
-        ];
+        if (argument && identity) {
+          const newAttributes = [
+            t.jSXAttribute(
+              t.jSXIdentifier('data-component-id'),
+              t.stringLiteral(identity),
+            ),
+          ];
 
-        argument.openingElement.attributes.push(...newAttributes);
+          if (argument) {
+            argument.openingElement.attributes.push(...newAttributes);
+          }
+        }
       },
     },
   };
